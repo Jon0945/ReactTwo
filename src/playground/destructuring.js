8d9c9286f5155d9edd6501c566563b93c29b9f6e
jon0945 <john.gullne@gmail.com>
2020-03-11 16:33:10 +0100

    END OF TUESDAY 0311

---
+++ src/playground/destructuring.js
@@ -0,0 +1,44 @@
+//
+//Object Destructuring
+//
+
+// const person = {
+//     name: 'Andrew',
+//     age: 26,
+//     location: {
+//         city: 'Philadelphia',
+//         temp: 92
+//     }
+// };
+
+// const {name: firstName = 'Anonymous', age} = person;
+// console.log(`${firstName} is ${age}. `);
+
+// const {city, temp: temperature} = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}. `);
+// }
+
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holliday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+
+
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName);
+
+//
+//Array Destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Philadelphia','Pennsylvania','19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffe (hot)', '$2.00', '$2.50', '$2.75'];
+const [name, , medium] = item;
+console.log(`A medium ${name} costs ${medium}`);
